4d89026e1c2850b8d6715eea24b544da220c7113
Nataliia-Karpova <nata14586321>
2019-10-26 00:48:12 +0300

    Решила 5.7

--- Home_work_5/HW_5.7.py
+++ Home_work_5/HW_5.7.py
@@ -1,1 +1,24 @@
 # Разверните строку всеми известными вам способами
+
+# Способ 1
+
+s = 'agatacaca'
+print(s[::-1])
+
+# Способ 2
+
+def reverse_string1(s):
+    chars = list(s)
+    for i in range(len(s) // 2):
+        tmp = chars[i]
+        chars[i] = chars[len(s) - i - 1]
+        chars[len(s) - i - 1] = tmp
+    return ''.join(chars)
+
+
+data = reverse_string1('agatacaca')
+print(data)
+
+# Способ 3
+
+print(''.join(reversed('agatacaca')))
