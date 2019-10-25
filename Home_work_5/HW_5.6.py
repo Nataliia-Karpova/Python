ce8643301e68334eaa465033b0c73fd5d0f171a6
Nataliia-Karpova <nata14586321>
2019-10-26 00:17:50 +0300

    Решила задачу 5.4

---
+++ Home_work_5/HW_5.6.py
@@ -0,0 +1,47 @@
+# Сконвертируйте список, кортеж, сэт и строку друг в друга всеми возможными способами
+# (строка -> лист, строка -> кортеж, ...)
+
+# Из строки в лист, кортеж и set
+
+For_convertation = str(input("Введите строку для преобразования в лист:"))
+print(list(For_convertation))
+
+For_convertation = str(input("Введите строку для преобразования в кортеж:"))
+print(tuple(For_convertation))
+
+For_convertation = str(input("Введите строку для преобразования в set:"))
+print(set(For_convertation))
+
+# Из листа в строку, кортеж и set
+
+
+For_convertation = list(input("Введите лист для преобразования в строку:"))
+print(str(For_convertation))
+
+For_convertation = list(input("Введите лист для преобразования в кортеж:"))
+print(tuple(For_convertation))
+
+For_convertation = list(input("Введите лист для преобразования в set:"))
+print(set(For_convertation))
+
+# Из кортежа в строку, лист и set
+
+For_convertation = tuple(input("Введите кортеж для преобразования в строку:"))
+print(str(For_convertation))
+
+For_convertation = tuple(input("Введите кортеж для преобразования в лист:"))
+print(list(For_convertation))
+
+For_convertation = tuple(input("Введите кортеж для преобразования в set:"))
+print(set(For_convertation))
+
+# Из set в строку, лист и кортеж
+
+For_convertation = set(input("Введите set для преобразования в строку:"))
+print(str(For_convertation))
+
+For_convertation = set(input("Введите set для преобразования в лист:"))
+print(list(For_convertation))
+
+For_convertation = set(input("Введите set для преобразования в кортеж:"))
+print(tuple(For_convertation))
