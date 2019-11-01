bddbc48d17caf3de653184d0cb1c205d38c9d46c
Nataliia-Karpova <nata14586321>
2019-11-01 21:39:41 +0300

    Правки в задаче 6.1

--- Home_work_6/HW_6.1.py
+++ Home_work_6/HW_6.1.py
@@ -1,9 +1,7 @@
 # Сделайте функцию, принимающую лист и возвращающую выпрямленный лист (исходный не должен был измениться):
 flatten = ([1, [1, 2], 3, [[4, 5, [6]]]])
-
-
-
 def straightened(flatten):
-    import copy
-    straightened_sheet = copy.deepcopy(flatten)
+    return str(flatten).replace('[','').replace(']','')
+print(straightened(([1, [1, 2], 3, [[4, 5, [6]]]])))
+
 
