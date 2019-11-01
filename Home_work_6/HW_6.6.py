679f01357276b95364c0ba431c511ec354e81865
Nataliia-Karpova <nata14586321>
2019-11-01 23:19:44 +0300

    Правки в задаче 6.6

--- Home_work_6/HW_6.6.py
+++ Home_work_6/HW_6.6.py
@@ -1,8 +1,6 @@
 # Функцию, находящую самый частый элемент в списке, если их несколько, то все
-moda([100, 3, 100, 2, 45, 45, 45, 3, 45, 3, 3, -2, 4])
-(3, 45)
-moda
-# Способ 1
+from statistics import mode
+
 moda = [100, 3, 100, 2, 45, 45, 45, 3, 45, 3, 3, -2, 4]
 def most_frequent(moda):
     counter = 0
@@ -13,18 +11,6 @@ def most_frequent(moda):
             counter = number_frequency
             most_freq = most_freq.insert(i)
     return most_freq
-
 print(most_frequent(moda))
 
 
-# Способ 2
-import statistics
-from statistics import mode
-
-
-def most_common_number(moda):
-    return (mode(moda))
-
-
-moda = [100, 3, 100, 2, 45, 45, 45, 3, 45, 3, 3, -2, 4]
-print(most_common_number(n))
