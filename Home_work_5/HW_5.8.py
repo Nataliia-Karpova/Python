1bffd8d31551bf266bf8392a1775c91d318ae7b9
Nataliia-Karpova <nata14586321>
2019-11-01 21:33:17 +0300

    Правки в задаче 5.6

--- Home_work_5/HW_5.8.py
+++ Home_work_5/HW_5.8.py
@@ -3,18 +3,33 @@
 
 # Способ 1
 
-DNA = str(input("Введите строку ДНК: "))
+DNA = input("Введите строку ДНК: ")
 DNA1 = DNA[::-1]
 DNA2 = DNA1.upper()
 DNA3 = DNA2.replace('A', 'W')
-DNA4 = DNA3.replace('G', 'C')
+DNA4 = DNA3.replace('G', 'X')
 DNA5 = DNA4.replace('T', 'A')
-DNA6 = DNA5.replace('C', 'X')
-DNA7 = DNA6.replace('X', 'G')
+DNA6 = DNA5.replace('C', 'G')
+DNA7 = DNA6.replace('X', 'C')
 DNA8 = DNA7.replace('W', 'T')
 print(DNA8)
 
 # Способ 2
+DNA = str(input("Введите строку ДНК: "))
+for i in range(n):
+    DNA1 = DNA.upper()
+    if i == 'A' or i == 'T':
+        if i == 'A':
+            DNA1 = DNA1.replace('A', 'T')
+        else:
+            DNA1 = DNA1.replace('T', 'A')
+    elif i == 'G' or i == 'C':
+        if i == 'G':
+            DNA1 = DNA1.replace('G', 'C')
+        else:
+            DNA1 = DNA1.replace('C', 'G')
+print(DNA1)
+
 
 
 DNA = str(input("Введите строку ДНК: "))
--- Home_work_6/HW_6.1.py
+++ Home_work_6/HW_6.1.py
@@ -1,3 +1,9 @@
 # Сделайте функцию, принимающую лист и возвращающую выпрямленный лист (исходный не должен был измениться):
 flatten = ([1, [1, 2], 3, [[4, 5, [6]]]])
-import copy
+
+
+
+def straightened(flatten):
+    import copy
+    straightened_sheet = copy.deepcopy(flatten)
+
