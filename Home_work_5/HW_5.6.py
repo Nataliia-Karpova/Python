1af831f00d3df0a492e15f6d44b1d6a6ded5738b
Nataliia-Karpova <nata14586321>
2019-11-01 20:20:56 +0300

    Правки в задаче 5.6

--- Home_work_5/HW_5.6.py
+++ Home_work_5/HW_5.6.py
@@ -3,18 +3,18 @@
 
 # Из строки в лист, кортеж и set
 
-For_convertation = str(input("Введите строку для преобразования в лист:"))
+For_convertation = input("Введите строку для преобразования в лист: ")
 print(list(For_convertation))
 
-For_convertation = str(input("Введите строку для преобразования в кортеж:"))
+For_convertation = input("Введите строку для преобразования в кортеж: ")
 print(tuple(For_convertation))
 
-For_convertation = str(input("Введите строку для преобразования в set:"))
+For_convertation = input("Введите строку для преобразования в set: ")
 print(set(For_convertation))
 
 # Из листа в строку, кортеж и set
 
-For_convertation = list(input("Введите лист для преобразования в строку:"))
+For_convertation = list(input("Введите лист для преобразования в строку: "))
 print(str(For_convertation))
 
 For_convertation = list(input("Введите лист для преобразования в кортеж:"))
